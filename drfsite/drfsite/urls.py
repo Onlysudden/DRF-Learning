5a6c0cf5e4df2a7ad7d0f2c4d033a267010cd900
Onlysudden <trofimof09@mail.ru>
2022-09-05 23:15:03 +0300

    Finish 12,13,14,15 lesson

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from women.views import WomenAPIList, WomenAPIUpdate, WomenAPIDestroy
 from rest_framework import routers
@@ -56,6 +57,9 @@ urlpatterns = [
     path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     # path('api/v1/', include(router.urls)),
     # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
     # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -4,21 +4,30 @@ from rest_framework import generics, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Women, Category
 from .serializers import WomenSerializer
 from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 
+class WomenApiListPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size' # Ручная регулировка количества данных
+    max_page_size = 10000 # Но не более этого числа
+
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = WomenApiListPagination
 
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
-    permission_classes = (IsOwnerOrReadOnly, )
+    permission_classes = (IsAuthenticated, )
+    # authentication_classes = (TokenAuthentication, ) - Убрали потому что JWT
 
 class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = Women.objects.all()
