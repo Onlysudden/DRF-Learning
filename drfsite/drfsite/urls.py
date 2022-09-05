cf891c006e7c40ada4df45ed35dd5059362be081
Onlysudden <trofimof09@mail.ru>
2022-09-05 15:18:48 +0300

    Finish 7,8,9 lesson

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -13,13 +13,40 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include
 from django.contrib import admin
 from django.urls import path
 
-from women.views import WomenAPIList
+from women.views import WomenViewSet
+from rest_framework import routers
+
+"""Правильно создавать файл routers и импортировать оттуда класс"""
+class MyCustomRouter(routers.SimpleRouter):
+    routes = [
+        routers.Route(
+            url=r'^{prefix}/$',
+            mapping={'get': 'list'},
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}),
+        routers.Route(
+            url=r'^{prefix}/{lookup}/$',
+            mapping={'get': 'retrieve'},
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Detail'})
+    ]
+
+#router = routers.SimpleRouter
+#router = routers.SimpleRouter - Есть первичная ссылка со списком url адресов
+router = MyCustomRouter()
+#router.register(r'women', WomenViewSet, basename='womens') - basenamen обязателен, если нету queryset
+router.register(r'women', WomenViewSet, basename='women')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist', WomenAPIList.as_view()),
-    path('api/v1/womenlist/<int:pk>/', WomenAPIList.as_view()),
+    path('api/v1/', include(router.urls)),
+    # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
+    # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
+    # path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
 ]
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.forms import model_to_dict
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
-from .models import Women
+from .models import Women, Category
 from .serializers import WomenSerializer
 
-class WomenAPIList(generics.ListCreateAPIView):
-    queryset = Women.objects.all()
+class WomenViewSet(viewsets.ModelViewSet):
+    #queryset = Women.objects.all() - Выбор всех полей, для работы нужно добавить basename в urls
     serializer_class = WomenSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+
+        if not pk:
+            return Women.objects.all()[:3]
+
+        return Women.objects.filter(pk=pk) # get_queryset должен возвращать список, поэтому берем запись через фильтр
+
+    @action(methods=['get'], detail=True) # detail=False выключает поиск по id
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': [cats.name]})
 
 """
 Пример через базовый класс APIView
@@ -49,4 +63,19 @@ class WomenAPIView(APIView):
         # Код для удаления записи
 
         return Response({'delete': 'delete post ' + str(pk)})
+"""
+
+"""
+Пример через специальные классы APIView
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+class WomenAPIUpdate(generics.UpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
 """
